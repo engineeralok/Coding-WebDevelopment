7c2d99a2f9e8d4f780834426548f38b262782bb9
engineeralok <rebelku1@gmail.com>
2021-05-17 21:37:57 +0530

    index.html and console.js

---
+++ javaScript/console.js
@@ -0,0 +1,12 @@
+//console.log()-> for print a massage in console log
+
+console.log("Hello JavaScript");
+console.log("Print an array:- "+[23,3,45,576]);
+console.log("Print an integer value:- " + 32);
+console.log("Print a boolean value:- " +true);
+console.log("Print an object:- " + {name:'Alok',Age:18,Branch:'ITESM'});
+
+//console.table()-> Prints a whole object as a table.
+
+console.table({name:'Alok',Age:18,Branch:'ITESM'});
+
