279a1789af2ddc2f2fbe5b6c44abbd5ceff96cfb
engineeralok <rebelku1@gmail.com>
2021-05-30 11:54:15 +0530

    6switch renamed 6switch.js and finished

---
+++ javaScript/6switch.js
@@ -0,0 +1,23 @@
+//Switch case statement
+// The switch statement is used to perform different actions based on different conditions.
+//compares multiple values
+
+let age = 20 ;
+switch (age){
+    case 18 :
+        console.log("you are 18");
+        break;
+
+    case 19:
+        console.log("you are 19");
+        break;
+
+    case 20:
+        console.log("you are 20");
+        break;
+
+    default:
+        console.log("you are above than 20");
+        break;
+
+}
