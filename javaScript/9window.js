1638aa484690c6dcbcc47dfe59f08da2fc1daf2b
engineeralok <rebelku1@gmail.com>
2021-06-01 22:15:00 +0530

    9window.js created and finished

---
+++ javaScript/9window.js
@@ -0,0 +1,30 @@
+//window object 
+//alert() -> shows a alert message in a dialogue box
+alert("hello World");
+
+//prompt() -> shows a dialogue box and take input from the user
+let a = prompt("enter your name");
+console.log(a);
+
+//confirm() -> shows a message in a dialogue box and assign a boolean value 
+
+confirm("do you want to exit");
+
+//window.innerHeight -> tells the current height of the browser window
+
+let height = window.innerHeight;
+console.log(height);
+
+//window.innerWidth -> tells the current width of the browser window 
+let width = window.innerWidth;
+console.log(width);
+
+//window.scrollX -> tells the distance or value of horizontal scrolling 
+let sx = window.scrollX;
+console.log(sx);
+
+//windows.scrollY -> tell the distance or value of vertical scrolling
+let sy = window.scrollY;
+console.log(sy);
+
+
