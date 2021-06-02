5191bcd9f28e53fd07811c436d8f385dffc2dfa0
engineeralok <rebelku1@gmail.com>
2021-06-02 16:24:32 +0530

    11dom.js finished

--- javaScript/11dom.js
+++ javaScript/11dom.js
@@ -9,3 +9,16 @@ let forms = document.forms[0];
 console.log(forms);
 
 //document.link -> to get links of the HTML document which is present in the page 
+let links = document.links;
+console.log(links);
+
+//document.images -> it will return all HTML <img> tag
+let img = document.images;
+console.log(img);
+
+//document.scripts -> it will return all html <script> element
+let script = document.scripts;
+console.log(script);
+
+//to access document properties 
+// -> document.propertyName
