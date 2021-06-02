12ec170a400e4ab6ceaf8f445d0bace5d1ed6156
engineeralok <rebelku1@gmail.com>
2021-06-02 10:10:22 +0530

    11dom.js modified

---
+++ javaScript/11dom.js
@@ -0,0 +1,11 @@
+// Document Object Model 
+//document.all -> shows all element of HTML document
+let htmlEle = document;
+htmlEle = document.all;
+console.log(htmlEle);
+
+//document.forms -> to get the forms from HTML document
+let forms = document.forms[0];
+console.log(forms);
+
+//document.link -> to get links of the HTML document which is present in the page 
