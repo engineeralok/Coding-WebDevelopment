9691ca5aa8fa675e774d3d478cea40a09acb88f7
engineeralok <rebelku1@gmail.com>
2021-06-08 20:37:02 +0530

    13elementSelector.js modified

--- javaScript/13elementSelector.js
+++ javaScript/13elementSelector.js
@@ -41,3 +41,124 @@ console.log(elementTwo);
 
 //Multiple element selector:-
 //It is use to select multiple HTML elements within a document. There are three ways in which we can select elements in a Dom using multiple element slectors.
+ //querySelectorAll() 
+ //getElementsByTagName()
+ //getElementsByClassName()
+
+ //getElementByClassName() -> This will select all the elements of the given classname.
+let cName = document.getElementsByClassName('h1Heading');
+console.log(cName);
+
+//getElementByTagName() -> Select all the elements of the given tagname.
+let tName = document.getElementsByTagName('a');
+console.log(tName);
+
+//Itarate of all the element of given tagname 'div'
+// let div = document.getElementsByTagName('div');
+// Array.from(div).forEach(element => {
+//     div = div.style.color = "blue";
+// });
+
+//Traversign the DOM :-
+// which means "move through", are used to "find" (or select) HTML elements based on their relation to other elements. Start with one selection and move through that selection until you reach the elements you desire.
+
+//.children -> Work as child nodes but exclude, comments, newline etc. It only shows elements.
+let cont = document.querySelector('.h1Heading');
+console.log(cont);
+console.log(cont.children);
+
+//.nodeName -> To find the nodename of the given index.
+let nodeName = cont.childNodes[1].nodeName;
+console.log(nodeName);
+
+//.nodeType -> find the type of the given node.
+//1 = element
+//2 = attribule
+//3 = text node
+//8 = comments
+//9 = document
+//10 = doctype
+//11 = a documentFragment node
+
+let nodeType = cont.childNodes[0].nodeType;
+console.log(nodeType);
+
+let container = document.querySelector('div.C');
+console.log(container.children[0].children);
+
+//.firstChild -> get the first child of selected element
+console.log(container.firstChild);
+
+//.firstElementChild -> get the first child element 
+console.log(container.firstElementChild);
+
+//.lastChild -> Get the last child element of the selected element
+console.log(container.lastChild);
+
+//.lastElementChile -> get the last child element of the selected element
+console.log(container.lastElementChild);
+
+//.childElementCount -> counts all the child elements
+console.log(container.childElementCount);
+
+//Traversing upword :-
+//.parentElement -> gets the parent elemen of the selected element
+console.log(container.parentElement);
+
+//Traversing sideways :-
+//.nextSibling -> gets the next sibling of the child
+console.log(container.nextSibling);
+
+//.nextElementSibling -> gets the next element sibling of the child 
+console.log(container.nextElementSibling);
+
+//.previousElementSibling -> to select previous element of the selected element
+console.log(container.previousElementSibling);
+
+//Creating, Removing & Replacing Elements :-
+//createElement() -> creates a element with given tagname.
+let newElement = document.createElement('li');
+
+//give id and class to the created element
+//.className -> give a class name to the created element
+newElement.className = "liClass";
+
+//.id -> give a id to the created element
+newElement.id = "liID";
+console.log(newElement);
+
+//setAttribute() -> give a attribute and its value to the created element
+newElement.setAttribute('tittle','created_Element');
+console.log(newElement);
+
+//.innerText -> to change or insert the text in a seleced element
+newElement.innerText = "hello i am a created li element";
+
+let ul = document.querySelector('.ulElement');
+console.log(ul);
+
+//apeendChild() -> appends or insert or put the created child i the selected element. Insert as a child 
+ul.appendChild(newElement);
+console.log(ul);
+
+//createTextNode() -> to create a text node
+let text = document.createTextNode('I am a text node append in new li element');
+newElement.appendChild(text);
+
+//create a new element and replace it with newElement
+let newHeading = document.createElement('h1');
+newHeading.id = 'NewH1id';
+newHeading.className = "NewH1class"
+let Tnode = document.createTextNode("This is a new Heading created with javaScript");
+newHeading.appendChild(Tnode);
+
+//replaceWith() -> replace an element with the new element
+newElement.replaceWith(newHeading);
+
+//Replace an id with new id :-
+cont.replaceChild('HeadinIDof',document.getElementsByClassName('h1Heading')[0].id);
+
+let idOfh1Heading = document.getElementsByClassName('h1Heading')[0].id;
+
+//replaceChild() -> Replace the child with new child
+
