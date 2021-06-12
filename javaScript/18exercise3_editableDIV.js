d90c1bd692f26a8baf0bfe37096913741fa81722
engineeralok <rebelku1@gmail.com>
2021-06-12 12:13:53 +0530

    18exercise3_editableDIV.js finished

--- javaScript/18exercise3_editableDIV.js
+++ javaScript/18exercise3_editableDIV.js
@@ -8,12 +8,19 @@
 let divElem = document.createElement('div');
 
 //set attributes to div
-divElem.setAttribute('id','elem');
-divElem.setAttribute('class','elem');
-divElem.setAttribute('style','border : 2px solid black; width : 100px; height : 50px; margin : 25px; padding : 10px');
+divElem.setAttribute('id', 'elem');
+divElem.setAttribute('class', 'elem');
+divElem.setAttribute('style', 'border : 2px solid black; width : 180px; height : 50px; margin : 25px; padding : 10px');
 
-//create text node
-let text = document.createTextNode('This is a editable div. Click to edit');
+//create text node and store text in local storage
+let textValue = localStorage.getItem('text');
+let text;
+if (textValue == null) {
+    text = document.createTextNode('This is a editable div. Click to edit');
+}
+else {
+    text = document.createTextNode(textValue);
+}
 
 //append or insert the text node under div
 divElem.appendChild(text);
@@ -26,7 +33,25 @@ let rbtn = document.getElementById('reloadButton');
 
 //insert the div element before rbtn which id is reloadButton
 //insertBefore -> used to insert the element before the given element
-container.insertBefore(divElem,rbtn);
+container.insertBefore(divElem, rbtn);
+
+//add event listener to the divElem
+divElem.addEventListener('click', function () {
+    //grab the length of the text area
+    let noTextArea = document.getElementsByClassName('textarea').length;
+    //if length of textarea is 0
+    if (noTextArea == 0) {
+        let html = elem.innerHTML;
+        divElem.innerHTML = `<textarea class="textarea form-control" id="textarea" rows="3"> ${html}</textarea>`;
+    }
+
+    //listen for the blur event on textarea
+    let textarea = document.getElementById('textarea');
+    textarea.addEventListener('blur', function () {
+        elem.innerHTML = textarea.value;
+        localStorage.setItem('text', textarea.value);
+    })
+});
 
 
 
